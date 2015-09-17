4369dfcee9b1d7f378ef74efa546d7e162c94774
Ludwig Magnusson <ludwig@mediatool.com>
2015-09-17 16:56:20 +0200

    Simplify structure of curry tests

--- test/curry.js
+++ test/curry.js
@@ -4,7 +4,6 @@ describe('curry', function () {
 
   var add2 = curry((a, b) => a + b)
   var add3 = curry((a, b, c) => a + b + c)
-  var isFunction = v => (typeof v).should.equal('function')
 
   var buildGreeting = curry(function (greeting) {
     return function (firstName, lastName) {
@@ -12,53 +11,24 @@ describe('curry', function () {
     }
   })
 
-  describe('m = 0', function () {
-
-    it('should return the input function', function () {
-      add2()(1)(2).should.equal(3)
-    })
-
+  it('should return the input function if given no arguments', function () {
+    add2()(1, 2).should.equal(3)
   })
 
-  describe('m = n', function () {
-
-    it('should call the function', function () {
-      add2(1, 2).should.equal(3)
-      add3(1, 2, 3).should.equal(6)
-    })
-
+  it('should call the function if given the correct number of arguments', function () {
+    add2(1, 2).should.equal(3)
+    add3(1, 2, 3).should.equal(6)
   })
 
-  describe('m < n', function () {
-
-    it('should return a function', function () {
-      isFunction(add2(1))
-      isFunction(add3(1))
-      isFunction(add3(1, 2))
-    })
-
-    it('should return a function that accepts the remaining arguments', function () {
-      add2(1)(2).should.equal(3)
-    })
-
-    it('n = 3', function () {
-      add3(1, 2)(3).should.equal(6)
-      add3(1)(2, 3).should.equal(6)
-      add3(1)(2)(3).should.equal(6)
-    })
-
+  it('should return a function that accepts the remaining arguments if given too few arguments', function () {
+    add2(1)(2).should.equal(3)
+    add3(1, 2)(3).should.equal(6)
+    add3(1)(2, 3).should.equal(6)
+    add3(1)(2)(3).should.equal(6)
   })
 
-  describe('m > n', function () {
-
-    describe('when result is a function', function () {
-
-      it('should curry the result and apply the remaining arguments', function () {
-        buildGreeting('Hello', 'John', 'Doe').should.equal('Hello John Doe!')
-      })
-
-    })
-
+  it('should curry the result and apply the remaining arguments when given too many arguments', function () {
+    buildGreeting('Hello', 'John', 'Doe').should.equal('Hello John Doe!')
   })
 
   it('should curry the result when it is a function', function () {
