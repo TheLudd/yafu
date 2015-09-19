0bc17422b347ca2addbace6208857cbb25ab1d99
Ludwig Magnusson <ludwig@mediatool.com>
2015-09-19 19:02:41 +0200

    Works, without slice

--- test/tail.js
+++ test/tail.js
@@ -1,17 +1,18 @@
 const tail = require('../lib/tail')
+const Maybe = require('../lib/maybe')
 
 describe('tail', function () {
 
-  it('should return a list with all elements except the initial', function () {
-    tail([ 1, 2, 3 ]).should.deep.equal([ 2, 3 ])
+  it('should return a Maybe of the list with all elements except the initial', function () {
+    Maybe.from([ 2, 3 ]).should.deep.equal(tail([ 1, 2, 3 ]))
   })
 
-  it('should return an empty list for a list of length 1', function () {
-    tail([ 1 ]).should.deep.equal([])
+  it('should return a Maybe of an empty list for a list of length 1', function () {
+    Maybe.from([]).should.deep.equal(tail([ 1 ]))
   })
 
-  it('should return an empty list for a list of length 0', function () {
-    tail([]).should.deep.equal([])
+  it('should return a Nothing for a list of length 0', function () {
+    Maybe.from(null).equals(tail([])).should.equal(true)
   })
 
 })
