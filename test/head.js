cb79cf77f926cae524ce20a366d0fed6e5153c81
Ludwig Magnusson <ludwig@mediatool.com>
2015-09-19 18:45:54 +0200

    Head uses maybe

--- test/head.js
+++ test/head.js
@@ -1,13 +1,15 @@
 const head = require('../lib/head')
+const Maybe = require('../lib/maybe')
 
 describe('head', function () {
 
-  it('should return the first element of a list', function () {
-    head([ 1, 2, 3 ]).should.equal(1)
+  it('should return a maybe of the first element of a list', function () {
+    Maybe.of(1).equals(head([ 1, 2, 3 ])).should.equal(true)
+    Maybe.of(1).equals(head([ 1 ])).should.equal(true)
   })
 
-  it('should return the only element of a list with length 1', function () {
-    head([ 1 ]).should.equal(1)
+  it('should return a maybe of nothing if the list is empty', function () {
+    Maybe.from(null).equals(head([])).should.equal(true)
   })
 
 })
