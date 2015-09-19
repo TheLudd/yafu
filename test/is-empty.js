bfff4e6c4d21d93801b7c067ec57b5f8d10db149
Ludwig Magnusson <ludwig@mediatool.com>
2015-09-19 18:58:04 +0200

    Add isEmpty

---
+++ test/is-empty.js
@@ -0,0 +1,15 @@
+module.exports = function (isEmpty) {
+
+  return function () {
+
+    it('should return true for an empty array', function () {
+      isEmpty([]).should.equal(true)
+    })
+
+    it('should return false for a non empty array', function () {
+      isEmpty([ 1 ]).should.equal(false)
+    })
+
+  }
+
+}
