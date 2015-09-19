3b84f143bc899a55c52470f1701dfacc370d6a75
Ludwig Magnusson <ludwig@mediatool.com>
2015-09-19 19:10:57 +0200

    Add drop
    [add] `drop`, a function that removes elements from the beginning of a
    list.

---
+++ test/drop.js
@@ -0,0 +1,17 @@
+module.exports = function (drop) {
+
+  return function () {
+
+    it('should remove the first n elements from a list', function () {
+      drop(1, [ 1, 2, 3 ]).should.deep.equal([ 2, 3 ])
+      drop(2, [ 1, 2, 3 ]).should.deep.equal([ 3 ])
+      drop(3, [ 1, 2, 3 ]).should.deep.equal([])
+    })
+
+    it('returns an empty list for n larger than the amount of elements in the list', function () {
+      drop(4, [ 1, 2, 3 ]).should.deep.equal([])
+    })
+
+  }
+
+}
