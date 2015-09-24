1938e146e2da3841a9a42314438cda05114770ff
Ludwig Magnusson <ludwig@mediatool.com>
2015-09-24 15:03:18 +0200

    Add reduce
    [add] The standard reduce function that will reduce a list of values to
    a single value.

---
+++ test/reduce.js
@@ -0,0 +1,17 @@
+module.exports = function (reduce) {
+
+  return function () {
+
+    const add = (a, b) => a + b
+
+    it('returns the initial argument for an empty list', function () {
+      reduce(null, 'X', []).should.equal('X')
+    })
+
+    it('performs the reduce operation on the array', function () {
+      reduce(add, 0, [ 1, 2, 3 ]).should.equal(6)
+    })
+
+  }
+
+}
