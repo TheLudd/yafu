2c952de38226440eb60a216e960c5ca5ebc2b26a
Ludwig Magnusson <ludwig@mediatool.com>
2016-04-01 19:32:08 +0200

    Add map
    [add] map: applies a function to each element in a list.

---
+++ test/map.js
@@ -0,0 +1,12 @@
+module.exports = function (map) {
+  return function () {
+    it('should return an empty array for empty input', function () {
+      map(null, []).should.deep.equal([])
+    })
+
+    it('should apply the mapping function to all values in the array', function () {
+      const inc = (x) => x + 1
+      map(inc, [ 1, 2, 3 ]).should.deep.equal([ 2, 3, 4 ])
+    })
+  }
+}
