58f3162d54fcbe9bb36004de36acffa1be4fbf0b
Ludwig Magnusson <ludwig@mediatool.com>
2016-10-27 20:26:46 +0200

    Add callBinaryWith
    [add]

---
+++ test/call-binary-with.js
@@ -0,0 +1,15 @@
+module.exports = function (callBinaryWith) {
+
+  return function () {
+
+    const subtract = (a, b) => a - b
+
+    it('should call a binary function with the provided arguments', () => {
+      const result = callBinaryWith(5, 7, subtract)
+      result.should.equal(-2)
+    })
+
+  }
+
+}
+
