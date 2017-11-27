3da3c76b8ba702aec865f4d01edc130ddcde0d8f
Ludwig Magnusson <ludwig@mediatool.com>
2017-11-27 18:28:09 +0100

    Clean up code, add parens

--- test/compose.js
+++ test/compose.js
@@ -1,7 +1,7 @@
 module.exports = function (compose) {
   return function () {
-    const inc = n => n + 1
-    const dbl = n => n * 2
+    const inc = (n) => n + 1
+    const dbl = (n) => n * 2
 
     it('composes right to left', function () {
       compose(inc, dbl, 20).should.equal(41)
