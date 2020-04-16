50e75bb690c485ae78be2cff488a127a01582d54
Ludwig Magnusson <ludwig@mediatool.com>
2020-04-16 18:06:10 +0200

    bundle project with rollup
    Old bundle files have been removed.

---
+++ build/create-index.js
@@ -0,0 +1,49 @@
+const { readdirSync, writeFileSync } = require('fs')
+const {
+  basename,
+  extname,
+  join,
+  normalize,
+} = require('path')
+const camelCase = require('camel-case')
+const mkdirp = require('mkdirp')
+
+function createIndex (projectPath) {
+  function getFileBaseName (fullName) {
+    const ext = extname(fullName)
+    return basename(fullName, ext)
+  }
+
+  function getAbsolute (relative) {
+    return normalize(join(projectPath, relative))
+  }
+
+  const jsFiles = readdirSync(getAbsolute('lib'))
+    .filter((s) => getFileBaseName(s).indexOf('_') !== 0)
+
+  const varNames = jsFiles
+    .map(getFileBaseName)
+    .map((s) => s.length === 1 ? s.toUpperCase() : camelCase(s))
+
+  const imports = jsFiles.map((item, i) => {
+    const pathName = `./${getFileBaseName(item)}`
+    const varName = varNames[i]
+    return `import ${varName} from './lib/${pathName}'`
+  }).join('\n')
+
+  const exports = varNames.map((varName) => {
+    return ` ${varName},`
+  }).join('\n')
+
+  const content = [
+    imports,
+    '',
+    'export {',
+    exports,
+    '}',
+  ].join('\n')
+  mkdirp.sync('dist')
+  writeFileSync(getAbsolute('index.js'), content)
+}
+
+createIndex(process.cwd())
---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import resolve from '@rollup/plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+
+export default {
+  input: 'index.js',
+  output: {
+    file: 'dist/yafu.js',
+    format: 'umd',
+    name: 'yafu',
+    sourcemap: true,
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+      presets: [
+        [ '@babel/preset-env', { modules: false } ],
+      ],
+    }),
+  ]
+}
